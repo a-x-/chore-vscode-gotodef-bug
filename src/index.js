58bfa219ed89365f2104c4d7225ccc087e2235ff
Alexander Mextner <a-x-@users.noreply.github.com>
2019-05-28 20:41:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // broken
 import {foo} from 'b';
-import {foo} from 'b/index';
-import {foo} from 'b/index.js';
+import {foo} from 'b/index'; // works on 1.34.0, but 1.35.0-insider
+import {foo} from 'b/index.js'; // works on 1.34.0, but 1.35.0-insider
 import {foo} from './b';
 
 // works
